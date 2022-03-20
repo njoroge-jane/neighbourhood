714e5e3f4074d64d1f599135f843c3a17c5e3709
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-20 19:20:46 +0300

    add models

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Neighbour(models.Model):
+  home_name = models.CharField()
+  location = models.CharField()
+  occupants_count = models.IntegerChoices()
+  admin = models.ForeignKey(User)
+
+class User(models.Model):
+  name = models.CharField()
+  id = models.IntegerField()
+  neighbour_id = models.ForeignKey(Neighbour)
+  email = models.EmailField()
+
+
+
+class Business(models.Model):
+  business_name = models.CharField()
+  user =  models.ForeignKey(User)
+  neighbour_id = models.ForeignKey(Neighbour)
+  business_email= models.EmailField()
+
+
