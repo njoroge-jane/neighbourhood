8bd04bcaa41d8bc039bccf094281f57142c38993
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-20 19:58:49 +0300

    register models in admin site

--- hood/urls.py
+++ hood/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('neighbour.urls')),
-    path('accounts/',include('registration.backends.simple.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('logout/', views.LogoutView.as_view(), {"next_page": ''}),
     path('tinymce/', include('tinymce.urls')),
 ]
--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Neighbour,Neighbourhood,Business
 
 # Register your models here.
+admin.site.register(Neighbour)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
