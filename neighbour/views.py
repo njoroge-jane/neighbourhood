e67ba5b518249f23866bdf3e75b761551868643d
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-21 20:26:45 +0300

    create a business view point

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+# from .forms import UploadImageForm,ProfileForm
+from .models import Business,Neighbour,Neighbourhood
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+  posts = Business.objects.all()
+
+
+  return render(request,'index.html', {"posts": posts})
